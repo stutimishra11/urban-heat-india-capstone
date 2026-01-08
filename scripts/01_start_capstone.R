d3d3fa4358bb43c4d596ca3bb1637dd54d94473d
stutimishra11 <stuti.mishra112@gmail.com>
2026-01-08 22:09:19 +0530

    Add initial analysis script

---
+++ scripts/01_start_capstone.R
@@ -0,0 +1,10 @@
+# Capstone Project: Urban Heat Intensity in Indian Cities
+# Author: Stuti Mishra
+
+# This script will be used to:
+# - load MODIS MOD11C3 land surface temperature data
+# - inspect variables and units
+# - convert temperature values from scaled Kelvin to Celsius
+# - prepare data for spatial and temporal analysis
+
+# Code will be added as preprocessing progresses
